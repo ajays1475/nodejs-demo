7aa75251b79f3599a35f75e3b496d9b219779deb
ajays1475 <ajays1475@gmail.com>
2018-04-09 22:06:51 +0530

    implemented production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,21 +1,35 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    //Hash the files using MD5 so that their name change when the content changes
+    new WebpackMd5Hash(),
+
+    /*Use CommonChunkPlugin to create a separate bundle
+    of vendor libraries so that they're cached separately*/
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
     //Create html file that references to Bundled JS
     new HtmlWebpackPlugin({
       template : 'src/index.html',
@@ -31,7 +45,10 @@ export default {
         minifyCSS: true,
         minifyURLs: true
       },
-      inject : true
+      inject : true,
+      //Properties we define here are available in index.html
+      //using htmlWebpackPlugin.options.varname
+      trackJSToken:'1ba68866e49149a09eb9dc23c131291a'
     }),
     //Plugin to remove duplicate package while bundling
     new webpack.optimize.DedupePlugin(),
@@ -41,7 +58,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }
